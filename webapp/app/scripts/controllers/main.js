4f3238c11111f4b1bd3c41aa9999e2f5ced9a049
Brian Blanchard <briblanch@gmail.com>
2014-11-04 10:48:57 -0700

    webapp-init: More clean up

--- webapp/app/scripts/controllers/main.js
+++ webapp/app/scripts/controllers/main.js
@@ -7,11 +7,8 @@
  * # MainCtrl
  * Controller of the capstone
  */
+
 angular.module('capstone')
     .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+        $scope.rooms = [];
     });
--- webapp/test/spec/controllers/about.js
+++ webapp/test/spec/controllers/about.js
@@ -17,7 +17,7 @@ describe('Controller: AboutCtrl', function () {
     }));
 
 
-  it('should attach a list of awesomeThings to the scope', function () {
+    it('should attach a list of awesomeThings to the scope', function () {
         expect(scope.awesomeThings.length).toBe(3);
     });
 });
