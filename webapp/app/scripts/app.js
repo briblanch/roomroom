3b80257e0cd09cd82f253958ed105519163653aa
Brian Blanchard <brianblanchard@LS2-00266.local>
2014-11-03 20:48:52 -0700

    webapp-init: Adding webapp scafolding

---
+++ webapp/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webappApp
+ * @description
+ * # webappApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('webappApp', [
+                'ngAnimate',
+                'ngCookies',
+                'ngResource',
+                'ngRoute',
+                'ngSanitize',
+                'ngTouch'
+        ])
+        .config(function ($routeProvider) {
+                $routeProvider
+                    .when('/', {
+                        templateUrl: 'views/main.html',
+                        controller: 'MainCtrl'
+                    })
+                    .when('/about', {
+                        templateUrl: 'views/about.html',
+                        controller: 'AboutCtrl'
+                    })
+                    .when('/testRoute', {
+                      templateUrl: 'views/testroute.html',
+                      controller: 'TestrouteCtrl'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+        });
