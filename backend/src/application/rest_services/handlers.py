48c48a20d627841e2aa16dda967c8ebc174f0775
derrickrahbar-wf <derrickrahbar@workiva.com>
2014-11-29 10:48:04 -0700

    wrote endpoint for get events

--- backend/src/application/rest_services/handlers.py
+++ backend/src/application/rest_services/handlers.py
@@ -1,6 +1,15 @@
 from application.models import Room
 from pprint import pprint
+from httplib2 import Http
 import json
+import datetime
+
+from oauth2client.client import SignedJwtAssertionCredentials
+from apiclient.discovery import build
+
+CLIENT_EMAIL = '152108792806-a3k1b8eso36ao5slv0atg7g7eaqa36ad@developer.gserviceaccount.com'
+GOOGLE_CAL_API = 'https://www.googleapis.com/auth/calendar'
+PRIVATE_KEY = open("keyStore.pem").read()
 
 def add_room(request_data):
 	room_to_add = Room(name=request_data['name'],
@@ -27,8 +36,7 @@ def get_rooms():
 
 	return rooms
 
-# What data is coming in to change? Will the request data have
-# all fields that should be on a room or just the ones its gonna update
+# Updates the info of a room with the info in the request
 def update_room(request_data, room_key):
 	print "DATA: ", request_data
 	print "KEY: ", room_key
@@ -43,4 +51,30 @@ def update_room(request_data, room_key):
 	room_to_update = room_to_update.to_dict()
 	room_to_update['id'] = str(room_key.id())
 	return room_to_update
+
+# Fetches all of the events from the google calendar api for a given room
+def get_room_events(request_data, room_key):
+	credentials = SignedJwtAssertionCredentials(CLIENT_EMAIL, PRIVATE_KEY, GOOGLE_CAL_API)
+	http_auth = credentials.authorize(Http())
+	cal_api = build('calendar', 'v3', http=http_auth)
+
+	room = Room.get_by_id(int(room_key))
+	cal_id = room.calendar
+	
+	time = datetime.date.today()
+	day = time.day
+	year = time.year
+	timeMin='%s-%s-25T00:00:00Z' % (year, day)  
+	timeMax='%s-%s-26T00:00:00Z' % (year, day)
+	
+	events_list = list()
+	page_token = None
+	while True:
+    	events = cal_api.events().list(cal_id, pageToken=page_token, timeMin=timeMin, timeMax=timeMax).execute()
+     	for event in events['items']:
+             events_list.append(event['summary'])
+     	page_token = events.get('nextPageToken')
+     	if not page_token:
+             break
+
 
