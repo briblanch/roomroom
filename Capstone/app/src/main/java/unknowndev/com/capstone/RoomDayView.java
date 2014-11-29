5adf53d89268d685a1596ca5f988458f3286b206
Kris Smith <kristophersmith@Kriss-MacBook-Pro.local>
2014-11-28 20:19:45 -0700

    ui-implementation: implemented list view

--- Capstone/app/src/main/java/unknowndev/com/capstone/RoomDayView.java
+++ Capstone/app/src/main/java/unknowndev/com/capstone/RoomDayView.java
@@ -3,6 +3,7 @@ package unknowndev.com.capstone;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,19 +11,39 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
-public class RoomDayView extends Activity {
+public class RoomDayView extends ListActivity {
+
+    static final ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_room_day_view);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                list,
+                R.layout.custom_row_view,
+                new String[] {"pen","price","color"},
+                new int[] {R.id.text1,R.id.text2, R.id.text3}
+        );
+
+        populateList();
+
+        setListAdapter(adapter);
+
+//        if (savedInstanceState == null) {
+//            getFragmentManager().beginTransaction()
+//                    .add(R.id.container, new PlaceholderFragment())
+//                    .commit();
+//        }
     }
 
 
@@ -46,6 +67,34 @@ public class RoomDayView extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void populateList() {
+        HashMap<String,String> temp = new HashMap<String,String>();
+        temp.put("pen","MONT Blanc");
+        temp.put("price", "200.00$");
+        temp.put("color", "Silver, Grey, Black");
+        list.add(temp);
+        HashMap<String,String> temp1 = new HashMap<String,String>();
+        temp1.put("pen","Gucci");
+        temp1.put("price", "300.00$");
+        temp1.put("color", "Gold, Red");
+        list.add(temp1);
+        HashMap<String,String> temp2 = new HashMap<String,String>();
+        temp2.put("pen","Parker");
+        temp2.put("price", "400.00$");
+        temp2.put("color", "Gold, Blue");
+        list.add(temp2);
+        HashMap<String,String> temp3 = new HashMap<String,String>();
+        temp3.put("pen","Sailor");
+        temp3.put("price", "500.00$");
+        temp3.put("color", "Silver");
+        list.add(temp3);
+        HashMap<String,String> temp4 = new HashMap<String,String>();
+        temp4.put("pen","Porsche Design");
+        temp4.put("price", "600.00$");
+        temp4.put("color", "Silver, Grey, Red");
+        list.add(temp4);
     }
 
     /**
